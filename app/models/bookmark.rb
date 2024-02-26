ff939b9f75da1c38464384813be92a1dd6143c60
Jose Carrillo <jacg182010@gmail.com>
2024-02-26 12:04:11 -0600

    Ejercicio 2 act

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :movie_id, uniqueness: { scope: :list_id,
-    message: "La pelicula ya se encuentra registrada" }
+                                     message: 'La película ya se encuentra registrada' }
   validates :comment, length: { minimum: 6 }
 end
